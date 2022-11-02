4172b027172995d1f06c6fcfe3d0f00dfe56784a
palomachcc <110131341+palomachcc@users.noreply.github.com>
2022-11-02 16:21:52 -0300

    Add files via upload

--- Basico/query_count.sql
+++ Basico/query_count.sql
@@ -10,3 +10,16 @@ select nombre, count (asignaturasid) as cant_asignaturas
 from Asignaturas
 where area='5'
 group by nombre
+
+--Tiene que tener sentido lo que consulto
+select nombre  
+from Asignaturas
+where area='5'
+group by nombre
+
+--lo siguiente no tiene mucho sentido, no se ejecuta porque si agrupas los registros por nombre, tenes mas de un ID para cada nombre...
+
+select nombre, AsignaturasID 
+from Asignaturas
+where area='5'
+group by nombre
