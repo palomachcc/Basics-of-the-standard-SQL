a6afafbb610533a9dbf8f4dfe17c19778b337556
palomachcc <110131341+palomachcc@users.noreply.github.com>
2022-11-06 16:17:05 -0300

    Update query_converison_datos.sql

--- Basico/query_converison_datos.sql
+++ Basico/query_converison_datos.sql
@@ -1,5 +1,7 @@
-----convertir datos
+----convertir datos-------
 
+
+--sintaxis para CONVERT: CONVERT(data_type(length), expression, style)
 select 
 [Fecha Ingreso],
 CONVERT(nvarchar(6),[Fecha Ingreso],112) AS YYYYMM
@@ -8,5 +10,15 @@ from Estudiantes
 --style:112	 yyyymmdd
 
 
+
+
+
+--sintaxis para CAST: CAST(expression AS datatype(length))
+
 select
 CAST(25.65 AS varchar);
+
+
+select
+cast(Costo*0.17 as decimal(7,3)) AS AUMENTO
+from asignaturas
